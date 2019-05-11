17077dc3ad0c2847a44c512d92262ba489e8bb74
Jenny <jennyelisa07@aol.com>
2019-05-10 23:09:58 -0400

    on.click function created

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,88 @@
-var randomNumber = []; // this is the random number generated at the start of each game. 
-var score; //this will push to number-box
-var crystals; //do i need each crystal to have its own number?
-var computerGuess= //number generated
+// $(document).ready( function() {
+var randomNumber = []; //  targetNumber this is the random number generated at the start of each game. 
+var score = 0; //this will push to number-box
+var crystals = []; //do i need each crystal to have its own number?
+var computerGuess; //number generated
+var numberOptions = [];
+var winsCounter = document.getElementById("wins");
+var lossesCounter = document.getElementById("losses");
+var crystalOne = document.getElementById("crystal-one");
+var crystalTwo = document.getElementById("crystal-two");
+var crystalThree = document.getElementById("crystal-three");
+var crystalFour = document.getElementById("crystal-four");
+    this.crystalOneNum = numberOptions[0];
+    this.crystalTwoNum = numberOptions[1];
+    this.crystalThreeNum = numberOptions[2];
+    this.crystalFourNum = numberOptions[3];
 
-function reset() {
-    randomNumber = score[Math.floor(Math.random() * letterPool.length)];
-    console.log(score);
+
+
+function restart() {
+    var crystalOneNum = crystalOne;
+    var crystalTwoNum = crystalTwo;
+    var crystalThreeNum = crystalThree;
+    var crystalFourNum = crystalFour;
+
+    for (let i = 19; i <= 120; i++) {
+        randomNumber.push(i);
+    }
+    computerGuess = randomNumber[Math.floor(Math.random() * randomNumber.length)];
+    console.log(computerGuess)
+
+    for (var j = 1; j <= 12; j++) {
+        crystals.push(j);
+    }
+
+    
+    for (var c = 0; c < 4; c++) {
+        numberOptions[c] = crystals[Math.floor(Math.random() * crystals.length)];
+    }
+    console.log(numberOptions);
+
+
+   
+
+    updateView();
 }
 
-$(document).ready(function(event) {
+function updateView() {
+    var numberBox = document.getElementById("number-box");
+    $(numberBox).text(computerGuess);
 
-for (randomNumber = 19; randomNumber < 120; randomNumber++){
-    $(randomNumber).push(".random-number");
 }
 
 
+$(this).on("click", function(event) {
+    // console.log(event.target);
+    var target = $(event.target.id);
+    // console.log(target);
+    
 
-
+    if (event.target.id == "crystal-one") {
+        score = score + numberOptions[0];
+    } else if (event.target.id == "crystal-two") {
+        score = score + numberOptions[1];
+    } else if (event.target.id == "crystal-three") {
+        score = score + numberOptions[2];
+    } else if (event.target.id == "crystal-four") {
+        score = score + numberOptions[3];
+    }
+ console.log(score)
 });
+
+function winLose () {
+    if (computerGuess === score) {
+
+    }
+}
+
+
+
+// console.log(crystals);
+
+restart();
+updateView();
+// function(event); 
+// console.log(randomNumber);
+
+// });
